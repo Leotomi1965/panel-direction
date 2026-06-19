b4255940b8f9da207460cd62893a71f28af86481
Leotomi1965 <rbariandaran@gmail.com>
2026-06-19 16:28:37 -0300

    ANDRA DEPLOY - actualización automática

---
+++ js/state.js
@@ -0,0 +1,13 @@
+export const STATE = {
+  empresas:[],tareas:[],notas:[],proyectos:[],
+  certificaciones:[],periodos:[],directivos:[]
+};
+
+export const app = {
+  CUR:'ARS',
+  vistaTareas:'tabla',
+  vistaProyectos:'tabla',
+  proyectoActivoId:null,
+  chartEERR:null,
+  exportFmt:'xlsx'
+};
